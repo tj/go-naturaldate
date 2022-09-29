feee37c3bbc64e6cc3e39028f787cb15ebb76db2
Issac Trotts <issac.trotts@protonmail.com>
2022-09-29 11:06:51 -0600

    extract TestParse_bad
    This contains a bunch of cases that are now considered invalid, with extra verbiage at the beginning.

--- naturaldate_test.go
+++ naturaldate_test.go
@@ -11,6 +11,50 @@ import (
 // base time.
 var base = time.Unix(1574687238, 0).UTC()
 
+// badCases are inputs that are expected to produce an error.
+var badCases = []string{
+	``,
+	`a`,
+	`not a date or a time`,
+	`right now`,
+	`  right  now  `,
+	`Message me in 2 minutes`,
+	`Message me in 2 minutes from now`,
+	`Remind me in 1 hour`,
+	`Remind me in 1 hour from now`,
+	`Remind me in 1 hour and 3 minutes from now`,
+	`Remind me in an hour`,
+	`Remind me in an hour from now`,
+	`Remind me one day from now`,
+	`Remind me in a day`,
+	`Remind me in one day`,
+	`Remind me in one day from now`,
+	`Message me in a week`,
+	`Message me in one week`,
+	`Message me in one week from now`,
+	`Message me in two weeks from now`,
+	`Message me two weeks from now`,
+	`Message me in two weeks`,
+	`Remind me in 12 months from now at 6am`,
+	`Remind me in a month`,
+	`Remind me in 2 months`,
+	`Remind me in a month from now`,
+	`Remind me in 2 months from now`,
+	`Remind me in one year from now`,
+	`Remind me in a year`,
+	`Remind me in a year from now`,
+	`Restart the server in 2 days from now`,
+	`Remind me on the 5th of next month`,
+	`Remind me on the 5th of next month at 7am`,
+	`Remind me at 7am on the 5th of next month`,
+	`Remind me in one month from now`,
+	`Remind me in one month from now at 7am`,
+	`Remind me on the December 25th at 7am`,
+	`Remind me at 7am on December 25th`,
+	`Remind me on the 25th of December at 7am`,
+	`Check logs in the past 5 minutes`,
+}
+
 // pastCases are test cases for the past direction.
 var pastCases = []struct {
 	Input  string
@@ -18,8 +62,6 @@ var pastCases = []struct {
 }{
 	// now
 	{`now`, `2019-11-25 13:07:18 +0000 UTC`},
-	{`right now`, `2019-11-25 13:07:18 +0000 UTC`},
-	{`  right  now  `, `2019-11-25 13:07:18 +0000 UTC`},
 
 	// minutes
 	{`1 minute`, `2019-11-25 13:06:18 +0000 UTC`},
@@ -32,8 +74,6 @@ var pastCases = []struct {
 	{`   5    minutes  ago   `, `2019-11-25 13:02:18 +0000 UTC`},
 	{`2 minutes from now`, `2019-11-25 13:09:18 +0000 UTC`},
 	{`two minutes from now`, `2019-11-25 13:09:18 +0000 UTC`},
-	{`Message me in 2 minutes`, `2019-11-25 13:09:18 +0000 UTC`},
-	{`Message me in 2 minutes from now`, `2019-11-25 13:09:18 +0000 UTC`},
 
 	// hours
 	{`1 hour`, `2019-11-25 12:07:18 +0000 UTC`},
@@ -42,11 +82,6 @@ var pastCases = []struct {
 	{`1 hour ago`, `2019-11-25 12:07:18 +0000 UTC`},
 	{`6 hours ago`, `2019-11-25 07:07:18 +0000 UTC`},
 	{`1 hour from now`, `2019-11-25 14:07:18 +0000 UTC`},
-	{`Remind me in 1 hour`, `2019-11-25 14:07:18 +0000 UTC`},
-	{`Remind me in 1 hour from now`, `2019-11-25 14:07:18 +0000 UTC`},
-	{`Remind me in 1 hour and 3 minutes from now`, `2019-11-25 14:10:18 +0000 UTC`},
-	{`Remind me in an hour`, `2019-11-25 14:07:18 +0000 UTC`},
-	{`Remind me in an hour from now`, `2019-11-25 14:07:18 +0000 UTC`},
 
 	// days
 	{`1 day`, `2019-11-24 00:00:00 +0000 UTC`},
@@ -55,10 +90,6 @@ var pastCases = []struct {
 	{`3 days ago`, `2019-11-22 00:00:00 +0000 UTC`},
 	{`3 days ago at 11:25am`, `2019-11-22 11:25:00 +0000 UTC`},
 	{`1 day from now`, `2019-11-26 13:07:18 +0000 UTC`},
-	{`Remind me one day from now`, `2019-11-26 13:07:18 +0000 UTC`},
-	{`Remind me in a day`, `2019-11-26 13:07:18 +0000 UTC`},
-	{`Remind me in one day`, `2019-11-26 13:07:18 +0000 UTC`},
-	{`Remind me in one day from now`, `2019-11-26 13:07:18 +0000 UTC`},
 
 	// weeks
 	{`1 week`, `2019-11-18 00:00:00 +0000 UTC`},
@@ -66,12 +97,6 @@ var pastCases = []struct {
 	{`2 weeks ago`, `2019-11-11 00:00:00 +0000 UTC`},
 	{`2 weeks ago at 8am`, `2019-11-11 08:00:00 +0000 UTC`},
 	{`next week`, `2019-12-02 00:00:00 +0000 UTC`},
-	{`Message me in a week`, `2019-12-02 13:07:18 +0000 UTC`},
-	{`Message me in one week`, `2019-12-02 13:07:18 +0000 UTC`},
-	{`Message me in one week from now`, `2019-12-02 13:07:18 +0000 UTC`},
-	{`Message me in two weeks from now`, `2019-12-09 13:07:18 +0000 UTC`},
-	{`Message me two weeks from now`, `2019-12-09 13:07:18 +0000 UTC`},
-	{`Message me in two weeks`, `2019-12-09 13:07:18 +0000 UTC`},
 
 	// months
 	{`1 month ago`, `2019-10-25 13:07:18 +0000 UTC`},
@@ -84,11 +109,6 @@ var pastCases = []struct {
 	{`next 2 months`, `2020-01-25 13:07:18 +0000 UTC`},
 	{`2 months from now`, `2020-01-25 13:07:18 +0000 UTC`},
 	{`12 months from now at 6am`, `2020-11-25 06:00:00 +0000 UTC`},
-	{`Remind me in 12 months from now at 6am`, `2020-11-25 06:00:00 +0000 UTC`},
-	{`Remind me in a month`, `2019-12-25 13:07:18 +0000 UTC`},
-	{`Remind me in 2 months`, `2020-01-25 13:07:18 +0000 UTC`},
-	{`Remind me in a month from now`, `2019-12-25 13:07:18 +0000 UTC`},
-	{`Remind me in 2 months from now`, `2020-01-25 13:07:18 +0000 UTC`},
 
 	// years
 	{`last year`, `2018-11-25 13:07:18 +0000 UTC`},
@@ -97,9 +117,6 @@ var pastCases = []struct {
 	{`one year from now`, `2020-11-25 13:07:18 +0000 UTC`},
 	{`two years ago`, `2017-11-25 13:07:18 +0000 UTC`},
 	{`2 years ago`, `2017-11-25 13:07:18 +0000 UTC`},
-	{`Remind me in one year from now`, `2020-11-25 13:07:18 +0000 UTC`},
-	{`Remind me in a year`, `2020-11-25 13:07:18 +0000 UTC`},
-	{`Remind me in a year from now`, `2020-11-25 13:07:18 +0000 UTC`},
 
 	// today
 	{`today`, `2019-11-25 00:00:00 +0000 UTC`},
@@ -191,14 +208,6 @@ var pastCases = []struct {
 	{`December 23rd AT 5:25 PM`, `2018-12-23 17:25:00 +0000 UTC`},
 	{`next December 23rd AT 5:25 PM`, `2019-12-23 17:25:00 +0000 UTC`},
 
-	// QA
-	{`Restart the server in 2 days from now`, `2019-11-27 13:07:18 +0000 UTC`},
-	{`Remind me on the 5th of next month`, `2019-12-05 13:07:18 +0000 UTC`},
-	{`Remind me on the 5th of next month at 7am`, `2019-12-05 07:00:00 +0000 UTC`},
-	{`Remind me at 7am on the 5th of next month`, `2019-12-05 07:00:00 +0000 UTC`},
-	{`Remind me in one month from now`, `2019-12-25 13:07:18 +0000 UTC`},
-	{`Remind me in one month from now at 7am`, `2019-12-25 07:00:00 +0000 UTC`},
-
 	// errors
 	{`10:am`, "\nparse error near PegText (line 1 symbol 1 - line 1 symbol 3):\n\"10\"\n"},
 }
@@ -224,10 +233,26 @@ var futureCases = []struct {
 	{`last january`, `2019-01-25 13:07:18 +0000 UTC`},
 	{`january`, `2020-01-25 13:07:18 +0000 UTC`},
 	{`next january`, `2020-01-25 13:07:18 +0000 UTC`},
-	{`Remind me on the December 25th at 7am`, `2019-12-25 07:00:00 +0000 UTC`},
-	{`Remind me at 7am on December 25th`, `2019-12-25 07:00:00 +0000 UTC`},
-	{`Remind me on the 25th of December at 7am`, `2019-12-25 07:00:00 +0000 UTC`},
-	{`Check logs in the past 5 minutes`, `2019-11-25 13:02:18 +0000 UTC`},
+	{`December 25th at 7am`, `2019-12-25 07:00:00 +0000 UTC`},
+	{`7am on December 25th`, `2019-12-25 07:00:00 +0000 UTC`},
+	{`25th of December at 7am`, `2019-12-25 07:00:00 +0000 UTC`},
+}
+
+// Test parsing with inputs that are expected to result in errors.
+func TestParse_bad(t *testing.T) {
+	for _, c := range badCases {
+		t.Run(c, func(t *testing.T) {
+			_, err := Parse(c, base)
+			if err == nil {
+				t.Errorf("err is %v, want nil", err)
+			}
+
+			_, err = Parse(c, base, WithDirection(Future))
+			if err == nil {
+				t.Errorf("future: err is %v, want nil", err)
+			}
+		})
+	}
 }
 
 // Test parsing with past direction.
