1efe90e7a6f94cb4961948822a7f0ebc7db94bd0
Issac Trotts <issac.trotts@protonmail.com>
2022-10-03 12:11:32 -0600

    fix test cases for 31/3/2014 and 31-3-2014

--- naturaldate_test.go
+++ naturaldate_test.go
@@ -319,8 +319,8 @@ func TestParse_goodDays(t *testing.T) {
 		{"2014-4", time.Date(2014, 4, 1, 0, 0, 0, 0, now.Location())},
 		{"31/3/2014 UTC-8", time.Date(2014, 3, 31, 0, 0, 0, 0, fixedZone(-8))},
 		{"31-3-2014 UTC-8", time.Date(2014, 3, 31, 0, 0, 0, 0, fixedZone(-8))},
-		{"31/3/2014", time.Date(2014, 3, 31, 0, 0, 0, 0, fixedZone(-8))},
-		{"31-3-2014", time.Date(2014, 3, 31, 0, 0, 0, 0, fixedZone(-8))},
+		{"31/3/2014", time.Date(2014, 3, 31, 0, 0, 0, 0, now.Location())},
+		{"31-3-2014", time.Date(2014, 3, 31, 0, 0, 0, 0, now.Location())},
 	}
 
 	for _, c := range cases {
