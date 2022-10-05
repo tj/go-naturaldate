1b1437defe53d91590b3ca2fa0272acc483a4347
Issac Trotts <issac.trotts@protonmail.com>
2022-10-04 21:25:36 -0600

    add Test_truncateWeek

--- naturaldate_test.go
+++ naturaldate_test.go
@@ -3,6 +3,7 @@ package naturaldate
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"testing"
 	"time"
 
@@ -396,6 +397,46 @@ func TestParse_bad(t *testing.T) {
 			v, err := Parse(c.input, now)
 			if err == nil {
 				t.Errorf("err is nil, result is %v", v)
+			}
+		})
+	}
+}
+
+func Test_truncateWeek(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want time.Time
+	}{
+		{
+			name: "sunday",
+			args: args{
+				t: time.Date(2022, 10, 2, 23, 59, 59, 999999, time.UTC),
+			},
+			want: time.Date(2022, 10, 2, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "monday",
+			args: args{
+				t: time.Date(2022, 10, 3, 23, 59, 59, 999999, time.UTC),
+			},
+			want: time.Date(2022, 10, 2, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "saturday",
+			args: args{
+				t: time.Date(2022, 10, 15, 23, 59, 59, 999999, time.UTC),
+			},
+			want: time.Date(2022, 10, 9, 0, 0, 0, 0, time.UTC),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := truncateWeek(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("truncateWeek() = %v, want %v", got, tt.want)
 			}
 		})
 	}
